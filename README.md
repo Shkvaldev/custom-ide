aceb052870cc6286eaf8396e48e4310f6ec8eaa2
ZaShkval <shkvalgrozny@gmail.com>
2024-01-17 23:19:07 +0500

    add some plugins + some settings

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
 _*Custom setup of neovim with lazy.nvim*_
 
+**Usage:**
+```shell
+:ToggleTerm # for running terminal
+```
+
 **Current list of plugins:**
 - _vim-code-dark (tomasiser/vim-code-dark)_
 - _which-key.nvim (folke/which-key.nvim)_
 - _plenary.nvim (nvim-lua/plenary.nvim)_
 - _telescope.nvim (nvim-telescope/telescope.nvim)_
+- _neo-tree.nvim (nvim-neo-tree/neo-tree.nvim)_
+- _toggleterm.nvim (akinsho/toggleterm.nvim)_
+- _barbar.nvim (romgrk/barbar.nvim)_
 
 TODO:
 - Setup keymaps
--- neovim-custom/configs/init.lua
+++ neovim-custom/configs/init.lua
@@ -28,7 +28,37 @@ local plugins = {
 	{
 		"nvim-telescope/telescope.nvim",
 	},
+	{
+    		"nvim-neo-tree/neo-tree.nvim",
+    		dependencies = {
+      			"nvim-lua/plenary.nvim",
+  			"nvim-tree/nvim-web-devicons",
+      			"MunifTanjim/nui.nvim",
+      		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+
+	},
+	{
+		'romgrk/barbar.nvim',
+    		dependencies = {
+            		'nvim-tree/nvim-web-devicons',
+		},
+    		init = function() vim.g.barbar_auto_setup = false end,
+    		opts = {
+			animation = true,
+    		},
+
+	},
 }
+
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+require("toggleterm").setup()
+
+vim.cmd(":set number")
+vim.cmd(":Neotree")
+vim.cmd(":set modifiable")
