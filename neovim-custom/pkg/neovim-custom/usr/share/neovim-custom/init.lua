d215c34a010e502cf8272e17803f3613bbd11559
Shkvaldev <svetlyak1033@gmail.com>
2024-03-23 10:35:25 +0000

    some reconfigure

---
+++ neovim-custom/pkg/neovim-custom/usr/share/neovim-custom/init.lua
@@ -0,0 +1,69 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy"
+if not vim.loop.fs_stat(lazypath) then
+	vim.notify("[!] Lazy plugin not found")
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{
+		"tomasiser/vim-code-dark",
+		lazy=false,
+		priority=1000,
+		config = function()
+			vim.cmd([[colorscheme codedark]])
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+		  vim.o.timeout = true
+		  vim.o.timeoutlen = 300
+		end,
+	},
+	{
+		"nvim-lua/plenary.nvim",
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+	},
+	{
+    		"nvim-neo-tree/neo-tree.nvim",
+    		dependencies = {
+      			"nvim-lua/plenary.nvim",
+  			"nvim-tree/nvim-web-devicons",
+      			"MunifTanjim/nui.nvim",
+      		},
+	},
+	{
+		"akinsho/toggleterm.nvim",
+
+	},
+	{
+		'romgrk/barbar.nvim',
+    		dependencies = {
+            		'nvim-tree/nvim-web-devicons',
+		},
+    		init = function() vim.g.barbar_auto_setup = false end,
+    		opts = {
+			animation = true,
+    		},
+
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		ft = { "markdown" },
+		build = function() vim.fn["mkdp#util#install"]() end,
+	},
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
+
+require("toggleterm").setup()
+
+vim.cmd(":set number")
+vim.cmd(":set modifiable")
